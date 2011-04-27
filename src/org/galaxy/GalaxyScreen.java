3eaf7a9d4be0e81351cc72aec940580c59105202
Marcus Lankenau <marcus.lankenau.ext@friendscout24.de>
2011-04-27 17:51:29 +0200

    game engine split up

--- src/org/galaxy/GalaxyScreen.java
+++ src/org/galaxy/GalaxyScreen.java
@@ -14,15 +14,10 @@ import android.view.View;
 
 public class GalaxyScreen extends View implements View.OnTouchListener {
 
-	ArrayList<Planet> planets = new ArrayList<Planet>();
-	ArrayList<Ship> ships = new ArrayList<Ship>();
-
+	Game game = new Game();
 	HashSet<Planet> dragFrom = new HashSet<Planet>();
 	Planet possibleTarget = null;
 
-	Party player = null;
-	Party computer = null;
-	Party neutral = null;
 
 	float fps = 15;
 
@@ -42,59 +37,9 @@ public class GalaxyScreen extends View implements View.OnTouchListener {
 	}
 
 	private void init() {
-		float boarder = 30;
-
-		Paint playerPaint = new Paint();
-		playerPaint.setColor(0xff00a000);
-		Paint computerPaint = new Paint();
-		computerPaint.setColor(0xffa00000);
-		Paint neutralPaint = new Paint();
-		neutralPaint.setColor(0xffa0a0a0);
-
-		player = new Party("player", playerPaint);
-		computer = new Party("computer", computerPaint);
-		neutral = new Party("", neutralPaint);
-
-		for (int i = 0; i < 6; i++) {
-			float size = 20;
-			int energy = 5;
-			float sr = (float) Math.random();
-			if (sr > 0.4) {
-				energy = 10;
-				size = 25;
-			}
-			if (sr > 0.7) {
-				energy = 20;
-				size = 30;
-			}
-
-			float x = 0;
-			float y = 0;
-			while (true) {
-				x = (float) (boarder + Math.random() * (320.f - 2.f * boarder));
-				y = (float) (boarder + Math.random() * (400.f - 2.f * boarder));
-
-				boolean collision = false;
-				for (Planet p : planets) {
-					Vector vp = p.getPos();
-					Vector dist = vp.sub(new Vector(x, y));
-					if (dist.length() < p.getSize() + size + 10.f)
-						collision = true;
-				}
-
-				if (!collision)
-					break;
-			}
-
-			Party party = neutral;
-			if (i == 0)
-				party = player;
-			if (i == 1)
-				party = computer;
-
-			planets.add(new Planet(party, new Vector(x, y), size, energy));
-		}
-
+		game.init();
+		
+		
 		Thread thread = new Thread(new Runnable() {
 			public void run() {
 				cycle();
@@ -114,29 +59,7 @@ public class GalaxyScreen extends View implements View.OnTouchListener {
 
 				Thread.sleep((int) (1000.f / fps));
 
-				for (Planet planet : planets) {
-					planet.grow((1.0f / fps));
-				}
-
-				ArrayList<Ship> deadShips = new ArrayList<Ship>();
-				for (Ship ship : ships) {
-					ship.move((1.0f / fps));
-
-					for (Planet planet : planets) {
-						if (planet.detectCollision(ship)
-								&& planet != ship.getSource()) {
-							
-							// if it is his own party and not destination, ignore
-							if (ship.getParty() == planet.getParty() && ship.getDest() != planet) continue;
-							
-							planet.collide(ship);
-							deadShips.add(ship);
-						}
-					}
-				}
-				for (Ship ship : deadShips) {
-					ships.remove(ship);
-				}
+				game.cycle(1.0f / fps);
 
 				((Activity) this.getContext()).runOnUiThread(new Runnable() {
 					public void run() {
@@ -158,7 +81,7 @@ public class GalaxyScreen extends View implements View.OnTouchListener {
 
 		Paint paintPlanetSelect = new Paint();
 		paintPlanetSelect.setColor(0x50ffffff);
-		for (Planet planet : planets) {
+		for (Planet planet : game.getPlanets()) {
 
 			canvas.drawCircle(planet.getPos().getX(), planet.getPos().getY(), planet.getSize(),
 					planet.getParty().getPaint());
@@ -171,7 +94,7 @@ public class GalaxyScreen extends View implements View.OnTouchListener {
 		  }
 			 
 		}
-		for (Ship ship : ships) {
+		for (Ship ship : game.getShips()) {
 			canvas.drawCircle(ship.getPos().getX(), ship.getPos().getY(), 2, ship.getParty()
 					.getPaint());
 		}
@@ -179,37 +102,30 @@ public class GalaxyScreen extends View implements View.OnTouchListener {
 		super.draw(canvas);
 	}
 
-	private Planet findPlanet(float x, float y, Party party) {
-		for (Planet planet : planets) {
-			if ((party == null || planet.getParty() == party)
-					&& planet.isHit(x, y, 30))
-				return planet;
-		}
-		return null;
-	}
+	
 
 	public boolean onTouch(View v, MotionEvent event) {
 		if (event.getAction() == MotionEvent.ACTION_DOWN) {
-			Planet planetDragFrom = findPlanet(event.getX(), event.getY(),
-					player);
+			Planet planetDragFrom = game.findPlanet(event.getX(), event.getY(),
+					game.getPlayer());
 			if (planetDragFrom != null)
 				dragFrom.add(planetDragFrom);
 
 		} else if (event.getAction() == MotionEvent.ACTION_MOVE) {
 			possibleTarget = null;
 			if (dragFrom.size() > 0) {
-				Planet planetDragFrom = findPlanet(event.getX(), event.getY(),
-						player);
+				Planet planetDragFrom = game.findPlanet(event.getX(), event.getY(),
+						game.getPlayer());
 				if (planetDragFrom != null)
 					dragFrom.add(planetDragFrom);
 
-				possibleTarget = findPlanet(event.getX(), event.getY(), null);
+				possibleTarget = game.findPlanet(event.getX(), event.getY(), null);
 			}
 		} else if (event.getAction() == MotionEvent.ACTION_UP) {
-			Planet dragTo = findPlanet(event.getX(), event.getY(), null);
+			Planet dragTo = game.findPlanet(event.getX(), event.getY(), null);
 			if (dragTo != null) {
 				for (Planet planetDragFrom : dragFrom) {
-					ships.addAll(planetDragFrom.launch(dragTo));
+					game.getShips().addAll(planetDragFrom.launch(dragTo));
 				}
 			}
 			dragFrom.clear();
