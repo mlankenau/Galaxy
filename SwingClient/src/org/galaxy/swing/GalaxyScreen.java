755d83bddf3801146218b34e10eff61adfb247e9
Marcus Lankenau <marcus.lankenau.ext@friendscout24.de>
2011-05-04 18:15:34 +0200

    swing client works now

--- SwingClient/src/org/galaxy/swing/GalaxyScreen.java
+++ SwingClient/src/org/galaxy/swing/GalaxyScreen.java
@@ -2,17 +2,28 @@ package org.galaxy.swing;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.util.HashSet;
 
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 import org.galaxy.Game;
 import org.galaxy.Planet;
+import org.galaxy.Ship;
 
 
 
-public class GalaxyScreen extends JPanel {
+public class GalaxyScreen extends JPanel implements MouseListener, MouseMotionListener {
 	Game game;
 	int fps = 10;
+	
+	HashSet<Planet> dragFrom = new HashSet<Planet>();
+	Planet possibleTarget = null;
+	
 	
 	public GalaxyScreen() {
 		super();
@@ -21,19 +32,32 @@ public class GalaxyScreen extends JPanel {
 		game = new Game();
 		game.init();
 		
+		addMouseListener(this);		
+		addMouseMotionListener(this);
+	
+		this.setDoubleBuffered(true);
+		
+		Thread thread = new Thread(new Runnable() {
+			public void run() {
+				cycle();
+			}
+		});
+		thread.start();
 	}
-
+	
 	@Override
 	public void paint(Graphics g) {
+		super.paint(g);
 		for (Planet planet : game.getPlanets()) {
 			int x = (int) planet.getPos().getX();
 			int y = (int) planet.getPos().getY();
 			int size = (int) planet.getSize();
-	        g.setColor(Color.white);
-
-			g.drawOval(x, y, size, size);
-//			canvas.drawText("" + planet.getEnergy(), planet.getPos().getX(),
-//					planet.getPos().getY() + 3, paintText);
+	        
+			g.setColor(new Color(planet.getParty().getColor()));
+			g.fillOval(x-size, y-size, 2*size, 2*size);
+			g.setColor(Color.white);
+			g.drawString("" + planet.getEnergy(), -5 + (int) planet.getPos().getX(), (int) planet.getPos().getY() + 3);
+			
 //
 //			if (dragFrom.contains(planet) || planet == possibleTarget) {
 //				canvas.drawCircle(planet.getPos().getX(), planet.getPos()
@@ -41,18 +65,19 @@ public class GalaxyScreen extends JPanel {
 //			}
 
 		}
-//		for (Ship ship : game.getShips()) {
-//			canvas.drawCircle(ship.getPos().getX(), ship.getPos().getY(), 2,
-//					getPaint(ship.getParty().getColor()));
-//		}
+		for (Ship ship : game.getShips()) {
+			g.setColor(new Color(ship.getParty().getColor()));
+			g.fillOval((int) ship.getPos().getX()-1, (int) ship.getPos().getY()-1, 4, 4);					
+		}
 		
-		super.paint(g);
+		
 	}
 	
 	/**
 	 * main thread loop
 	 */
 	private void cycle() {
+		
 		try {
 			while (true) {
 
@@ -60,11 +85,16 @@ public class GalaxyScreen extends JPanel {
 
 				game.cycle(1.0f / fps);
 
-				new Runnable() {
+				Runnable redraw = new Runnable() {
 					public void run() {
-						invalidate();
+						//Graphics g = getGraphics();
+						//paint(g);
+						//update(g);
+						
+						paintImmediately(0, 0, getWidth(), getHeight());
 					}
 				};
+				SwingUtilities.invokeLater(redraw);
 			}
 
 		} catch (InterruptedException e) {
@@ -72,34 +102,66 @@ public class GalaxyScreen extends JPanel {
 		}
 	}
 
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 
-//	@Override
-//	public void draw(Canvas canvas) {
-//		Paint paintText = new Paint();
-//		paintText.setColor(0xffffffff);
-//		paintText.setTextAlign(Align.CENTER);
-//
-//		Paint paintPlanetSelect = new Paint();
-//		paintPlanetSelect.setColor(0x50ffffff);
-//		for (Planet planet : game.getPlanets()) {
-//
-//			canvas.drawCircle(planet.getPos().getX(), planet.getPos().getY(), planet.getSize(),
-//					getPaint(planet.getParty().getColor()));
-//			canvas.drawText("" + planet.getEnergy(), planet.getPos().getX(),
-//					planet.getPos().getY() + 3, paintText);
-//			
-//		  if (dragFrom.contains(planet) || planet == possibleTarget) {
-//			  canvas.drawCircle(planet.getPos().getX(), planet.getPos().getY(),
-//					  planet.getSize()+15, paintPlanetSelect); 
-//		  }
-//			 
-//		}
-//		for (Ship ship : game.getShips()) {
-//			canvas.drawCircle(ship.getPos().getX(), ship.getPos().getY(), 2, getPaint(ship.getParty().getColor()));
-//		}
-//
-//		super.draw(canvas);
-//	}
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent event) {
+		Planet planetDragFrom = game.findPlanet(event.getX(), event.getY(),
+				game.getPlayer());
+		if (planetDragFrom != null)
+			dragFrom.add(planetDragFrom);
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent event) {
+		Planet dragTo = game.findPlanet(event.getX(), event.getY(), null);
+		if (dragTo != null) {
+			for (Planet planetDragFrom : dragFrom) {
+				System.out.println("Launching sups... ");
+				game.getShips().addAll(planetDragFrom.launch(dragTo));
+			}
+		}
+		dragFrom.clear();
+		possibleTarget = null;
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent event) {
+		possibleTarget = null;
+		if (dragFrom.size() > 0) {
+			Planet planetDragFrom = game.findPlanet(event.getX(), event.getY(),
+					game.getPlayer());
+			if (planetDragFrom != null)
+				dragFrom.add(planetDragFrom);
+
+			possibleTarget = game.findPlanet(event.getX(), event.getY(), null);
+		}		
+	}
+
+
 
 	
 }
