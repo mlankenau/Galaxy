06425789a046080f8535b791d1b39a7aad4381e8
Marcus <Marcus@geheim.de>
2011-05-08 16:05:32 +0200

    udpmonitor works now

--- UdpMonitor/udpmon.rb
+++ UdpMonitor/udpmon.rb
@@ -1,7 +1,9 @@
 require 'socket'
  s2 = UDPSocket.new
- s2.bind("127.0.0.1", 0)
- s2.connect(*s1.addr.values_at(3,1))
- IO.select([s2])
- p s2.recvfrom(10)  #=> ["aaa", ["AF_INET", 33302, "localhost.localdomain", "127.0.0.1"]]
+ s2.bind("127.0.0.1", 10001)
+ #IO.select([s2])
  
+while (true) 
+
+	p s2.recvfrom(10)
+end 
